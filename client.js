822632147a949a4cf032486b881bad30145c122b
satohinash <206806064+satohinash@users.noreply.github.com.>
2026-01-09 12:13:38 +0900

    変更

---
+++ client.js
@@ -0,0 +1,23 @@
+'use strict';
+
+function dataset() {
+  return {
+    signupUsername: '',
+    signupPassword: '',
+    loginUsername: '',
+    loginPassword: '',
+    result: 'ここに結果が表示されます...',
+
+    /* ユーザー登録 */
+    async signup() {},
+
+    /* ログイン */
+    async login() {},
+
+    /* プロフィールの取得 */
+    async getProfile() {},
+
+    /* ログアウト */
+    async logout() {}
+  };
+}
